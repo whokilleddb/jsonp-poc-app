08ec159e4155bdb66eb7b0124a3204fabdd4c4ec
whokilleddb <whokilleddb@protonmail.com>
2023-01-30 15:38:08 +0530

    Added verbosity

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ const PORT = 8000;
 const users = [{
     id: 1,
     username: 'farid',
-    password: '$2b$10$lq5kBLti.5p/Ziq6GmXVdeVr2mMJvyPvFk.zpiy.2F1ozVqTw18cu' // passw0rd
+    password: '$2b$10$lq5kBLti.5p/Ziq6GmXVdeVr2mMJvyPvFk.zpiy.2F1ozVqTw18cu', // passw0rd
+    team : 'rocket',
+    acc: 'XQC-MK-LUD'
 }];
 
 // Authenticate
@@ -39,7 +41,7 @@ function isAuthenticated(req, res, next) {
     if (req.session && req.session.user) {
         return next();
     }
-    return res.status(401).send({ message: 'Not authenticated' });
+    return res.redirect('/');
 }
 
 
@@ -47,10 +49,11 @@ function isAuthenticated(req, res, next) {
 app.post('/login', function(req, res) {
     const {username, password} = req.body;
     const user = users.find(u => u.username === username);
+    console.log("[i] Trying credentials - %s:%s", username, password)
 
     // Check if user exists
     if (!user) {
-        console.log("Failed Login attempt - %s:%s", username, password)
+        console.warn("[!] Could not find user: %s", username)
         return res.status(401).send({ message: 'Invalid Credentials' });
     }
 
@@ -58,30 +61,46 @@ app.post('/login', function(req, res) {
     bcrypt.compare(password, user.password, (err, result) => {
         if (result) {
             req.session.user = user;
-            console.log("Logged In As - %s:%s", username, password)
+            console.log("[i] Logged In As - %s", username)
             return res.send({status: "ok"});
         }
-        console.log("Failed Login attempt - %s:%s", username, password)
+        console.warn("[!] Failed to Login user - %s", username)
         return res.status(401).send({ message: 'Invalid Credentials' });
     });
 });  
 
-// Dashboard page
+
+// show user info
+app.get('/whoami', isAuthenticated, (re1, res) => {
+    user_info = {
+        id: users[0].id,
+        name: users[0].username,
+        team: users[0].team,
+        acc: users[0].acc,
+    };
+    console.log("[i] Served User Info")
+    return res.send(JSON.stringify(user_info));
+});
+
+// Console page
 app.get(['/console', '/console.html'], isAuthenticated, (req, res) => {
+    console.log("[i] Served Console")
     res.sendFile(path.join(__dirname, '/routes/console.html'));
 });  
 
 
 // Index page
 app.get(['/', '/index', '/index.html'], function(req, res) {
+    console.log("[i] Served index page")
     res.sendFile(path.join(__dirname, '/routes/index.html'));
 });  
 
 // 404 error
 app.get('*', function(req, res){
+    console.warn("[!] Invalid URL: %s", req.originalUrl)
     res.status(404).sendFile(path.join(__dirname, '/routes/404.html'));
 });
 
 app.listen(PORT, HOST, () => {
-    console.log('Server running at http://%s:%d/', HOST, PORT);
+    console.log('[i] Server running at http://%s:%d/', HOST, PORT);
   })
